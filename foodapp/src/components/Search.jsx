ed3bc99ea149a5110d8c356e533d603ee47c05a1
Vin-it-git <palvinit33@gmail.com>
2024-08-31 18:04:34 +0530

    second commit

--- foodapp/src/components/Search.jsx
+++ foodapp/src/components/Search.jsx
@@ -1,12 +1,18 @@
 import { useEffect, useState } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch"
+const API_KEY = "ad9ceb31c588442a9c2e87d97d4fbcc3"
 export default function Search() {
   const [query, setQuery] = useState("pizza");
   
+  
   useEffect(() => {
-    function demo(){
-        console.log("demo function executed")
+    async function fetchfood(){
+        const res =await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`)
+        const data =await res.json()
+        console.log(data.results)
     }
-    demo()
+    fetchfood()
   }, [query])
   return (
     <div>
